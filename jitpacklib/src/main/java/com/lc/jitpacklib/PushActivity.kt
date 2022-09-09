7f21d584c26670333965834bf5682a56a24dabb6
luochuang <luochuang@91160.com>
2022-09-09 10:58:41 +0800

    init

---
+++ jitpacklib/src/main/java/com/lc/jitpacklib/PushActivity.kt
@@ -0,0 +1,15 @@
+package com.lc.jitpacklib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PushActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push)
+    }
+
+    fun start() {
+        val c = 1
+    }
+}
